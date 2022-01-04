c5d78c1972ad2d090170c4cbfb55b34158e7ec0d
Juan Narvaez <juand.ns07@gmail.com>
2022-01-04 17:39:12 -0300

    Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import Logo from "../img/descarga.svg";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={Logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            React
+          </Navbar.Brand>
+          <Nav className="me-ritgh">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
